cbb5c29f9a2d0e1d7f4d6f1e3ce081a5c9561351
Vee Crichton-Hill <vcrichtonhill@gmail.com>
2022-04-29 22:12:05 -0500

    added questions

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const { type } = require('os');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a description for your project.'
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Enter installation instructions.'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Enter usage information.'
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Enter contribution guidelines.'
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Enter test instructions.'
+        }
+    ])
+}
+
+questions();
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
